179a0efb2172f757f40026782f55805de633f988
Rubensrvsc <Rubensspfc100@gmail.com>
2018-09-30 19:31:16 -0300

    ok

--- MotorBusca.py
+++ MotorBusca.py
@@ -1,55 +1,64 @@
 import requests
 from lxml.html import fromstring as parser
 from bs4 import BeautifulSoup
+import re
 
+list_palavras=[]
 def main():
     obter_pagina_e_profundidade()
     pass
 
 def obter_pagina_e_profundidade():
-    profundidade=input("digite a profundidade: ")
-    palavra=input("digite a palavra: ")
-    url="http://libra.ifpi.edu.br/a-instituicao/reitoria"
-    obter_links(profundidade,url,palavra)
+    print("------digite a palavra a pagina e a profundidade------")
+    profundidade=int(input("digite a profundidade: "))
+    if(profundidade==0):
+        palavra=input("digite a palavra: ")
+        url="http://libra.ifpi.edu.br/a-instituicao/reitoria"
+        obter_links(profundidade,url,palavra)
     pass
 
 
 def obter_links(profundidade,url,palavra):
     response=requests.get(url)
-    res = BeautifulSoup(response.text,"html.parser");
+    res = BeautifulSoup(response.text,"html.parser")
     l=[]
     for i in res.find_all("a"):
         l.append(i)
-    for j in range(len(l)):
-        procura_palavras(l[j],palavra)
+    ocorrencias(response,palavra)
+    encontra_palavras(response,palavra)
+    '''for j in range(len(l)):
+        procura_palavras(l[j],palavra)'''
     pass
 
-def procura_palavras(j,palavra):
+def procura_links(j,palavra):
+    list_links=''
     link=j.get("href").strip("http://")
     if(link.find("#")==0):
         print ("contem cerquilha")
     else:
         r=requests.get("http://"+link)
-        print(r.text)
+        bs=BeautifulSoup(r.text,"html.parser")
+        for i in bs.find_all("a"):
+            print(i.get("href"))
+            list_links+=i.get("href")
+    return list_links
     pass
+def encontra_palavras(response,palavra):
+    print(re.findall(palavra,response.text))
 
-'''def obter_paginas_dos_links(profundidade,site):
-    response=requests.get(site)
-    html = parser(response.text)
-    soup = BeautifulSoup(response.text,"lxml")
-    all_links=soup.find_all("a")
-    paginas=soup.find_all("body")
-    if(profundidade==0):
-        return paginas
-    else:
-        obter_paginas_dos_links(profundidade-1,site)
+def ocorrencias(pagina,palavra):
+    lista_palavras=re.finditer(palavra,pagina.text)
+    lista_match=[]
+    for i in lista_palavras:
+        lista_match.append(i.string)
+    print("Ocorrencias: %d" %(len(lista_match)))
     pass
-
 def encontrar_palavras(page,palavra):
-    lista_palavras=[]
+    lista_palavras=''
     if(page.__contains__(palavra)):
-        lista_palavras.append(palavra)
-    pass'''
+        lista_palavras+=palavra
+    return lista_palavras
+    pass
 if __name__=="__main__":
     main()
 
