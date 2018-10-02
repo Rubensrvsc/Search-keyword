01618ef81aa5e1ba4b33d011a1bda7e666334c8c
unknown <Rubensspfc100@gmail.com>
2018-10-02 10:56:00 -0300

    ajuste na busca de links

--- MotorBusca.py
+++ MotorBusca.py
@@ -11,10 +11,9 @@ def main():
 def obter_pagina_e_profundidade():
     print("------digite a palavra a pagina e a profundidade------")
     profundidade=int(input("digite a profundidade: "))
-    if(profundidade==0):
-        palavra=input("digite a palavra: ")
-        url="http://libra.ifpi.edu.br/a-instituicao/reitoria"
-        obter_links(profundidade,url,palavra)
+    palavra=raw_input("digite a palavra: ")
+    url="http://libra.ifpi.edu.br/a-instituicao/reitoria"
+    obter_links(profundidade,url,palavra)
     pass
 
 
@@ -22,15 +21,18 @@ def obter_links(profundidade,url,palavra):
     response=requests.get(url)
     res = BeautifulSoup(response.text,"html.parser")
     l=[]
-    for i in res.find_all("a"):
-        l.append(i)
-    ocorrencias(response,palavra)
-    encontra_palavras(response,palavra)
+    if(profundidade>0):
+        for i in res.find_all("a"):
+            procura_links(i,palavra)
+    if(profundidade==0):
+        ocorrencias(response,palavra)
+        #encontra_trecho(response.content,palavra)
+        print(encontrar_palavras(response.text,palavra))
     pass
 
-def procura_links(j,palavra):
-    list_links=''
-    link=j.get("href").strip("http://")
+def procura_links(links,palavra):
+    list_links=[]
+    link=links.get("href").strip("http://")
     if(link.find("#")==0):
         print ("contem cerquilha")
     else:
@@ -38,11 +40,23 @@ def procura_links(j,palavra):
         bs=BeautifulSoup(r.text,"html.parser")
         for i in bs.find_all("a"):
             print(i.get("href"))
-            list_links+=i.get("href")
-    return list_links
+            list_links.append(i.get("href"))
+    for j in list_links:
+        response=requests.get(j)
+        res = BeautifulSoup(response.text,"html.parser")
+        for h in res.find_all("a"):
+            procura_links(i)
     pass
-def encontra_palavras(response,palavra):
-    print(re.findall(palavra,response.text))
+'''def encontra_trecho(response,palavra):
+    lista=[]
+    for i in re.finditer(palavra.lower(),response.lower()):
+        print(tuple(i.groups()))
+        lista.append(i)
+    lista_palavras=[]
+    for j in lista:
+        lista_palavras.append(lista[-10:+10])
+    for h in range(len(lista_palavras)):
+        print(lista_palavras[h])'''
 
 def ocorrencias(pagina,palavra):
     lista_palavras=re.finditer(palavra,pagina.text)
@@ -51,11 +65,8 @@ def ocorrencias(pagina,palavra):
         lista_match.append(i.string)
     print("Ocorrencias: %d" %(len(lista_match)))
     pass
-def encontrar_palavras(page,palavra):
-    lista_palavras=''
-    if(page.__contains__(palavra)):
-        lista_palavras+=palavra
-    return lista_palavras
+def encontrar_palavras(response,palavra):
+    print(re.findall(palavra,response))
     pass
 
 if __name__=="__main__":
